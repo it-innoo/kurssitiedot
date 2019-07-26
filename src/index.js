24bb8f40df46bb01ae23f8098e25f0e2c14cb0fa
jailgreen <36865973+jailgreen@users.noreply.github.com>
2019-07-26 08:49:31 +0300

    Part 1.2

--- src/index.js
+++ src/index.js
@@ -10,18 +10,20 @@ const Header = (props) => {
   )
 }
 
+const Part = (props) => {
+  return (
+    <p>
+      {props.name} {props.count}
+    </p>
+  )
+}
+
 const Content = (props) => {
   return (
     <main>
-      <p>
-        {props.part1} {props.exercises1}
-      </p>
-      <p>
-        {props.part2} {props.exercises2}
-      </p>
-      <p>
-        {props.part3} {props.exercises3}
-      </p>
+      <Part name={props.part1} count={props.exercises1} />
+      <Part name={props.part2} count={props.exercises2} />
+      <Part name={props.part3} count={props.exercises3} />
     </main>
   )
 }
